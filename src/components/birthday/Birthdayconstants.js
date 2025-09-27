f1b6464675ebdcf292b1803b5506ac5bcde5fc9d
kundal kumar <kundal5335@gmail.com>
2025-09-28 01:52:31 +0530

    upload to github

--- src/components/birthday/Birthdayconstants.js
+++ src/components/birthday/Birthdayconstants.js
@@ -3,6 +3,12 @@ import aesthetic from "url:../../assets/img/birthdayframe/aesthetic.jpg";
 import asthetic from "url:../../assets/img/birthdayframe/asthetic.jpg";
 import happybirthday from "url:../../assets/img/birthdayframe/happy-birthday.jpg";
 import happybirthday4 from "url:../../assets/img/birthdayframe/happy-birthday-4.jpg";
+import happybirthday1 from "url:../../assets/img/birthdayframe/happy-birthday-1.jpg";
+import happybirthday2 from "url:../../assets/img/birthdayframe/happy-birthday-2.jpg";
+import happybirthday3 from "url:../../assets/img/birthdayframe/happy-birthday-3.jpg";
+import happybirthday5 from "url:../../assets/img/birthdayframe/happy-birthday-5.jpg";
+import happybirthday6 from "url:../../assets/img/birthdayframe/happy-birthday-6.jpg";
+
 const birthdayimg = [
     {
         pictureLink: birthdayFrame1,
@@ -178,7 +184,8 @@ const birthdayimg = [
             }
 
         ],
-    }, {
+    },
+    {
         pictureLink: happybirthday4,
         id: 5,
         category: "normal",
@@ -191,8 +198,8 @@ const birthdayimg = [
                 id: 1,
                 location: {
                     left: 119,
-                    top: 288, 
-                    rotate:-15
+                    top: 288,
+                    rotate: -15
 
                 },
                 reso: {
@@ -203,5 +210,185 @@ const birthdayimg = [
 
         ],
     },
+    {
+        pictureLink: happybirthday1,
+        id: 6,
+        category: "normal",
+        Reso: {
+            width: 736,
+            height: 1308
+        },
+        PictureLocation: [
+            {
+                id: 1,
+                location: {
+                    left: 129,
+                    top: 249
+                },
+                reso: {
+                    width: 480,
+                    height: 636
+                }
+            }
+
+        ],
+    },
+    {
+        pictureLink: happybirthday2,
+        id: 7,
+        category: "normal",
+        Reso: {
+            width: 736,
+            height: 1308
+        },
+        PictureLocation: [
+            {
+                id: 1,
+                location: {
+                    left: 30,
+                    top: 148
+                },
+                reso: {
+                    width: 242,
+                    height: 231
+                }
+            },
+            {
+                id: 2,
+                location: {
+                    left: 385,
+                    top: 360
+                },
+                reso: {
+                    width: 306,
+                    height: 296
+                }
+            },
+            {
+                id: 3,
+                location: {
+                    left: 45,
+                    top: 564
+                },
+                reso: {
+                    width: 289,
+                    height: 278
+                }
+            },
+            {
+                id: 4,
+                location: {
+                    left: 384,
+                    top: 790
+                },
+                reso: {
+                    width: 214,
+                    height: 205
+                }
+            }
+
+        ],
+    },
+    {
+        pictureLink: happybirthday3,
+        id: 8,
+        category: "normal",
+        Reso: {
+            width: 736,
+            height: 1308
+        },
+        PictureLocation: [
+            {
+                id: 1,
+                location: {
+                    left: 127,
+                    top: 332
+                },
+                reso: {
+                    width: 462,
+                    height: 624
+                }
+            }
+
+        ],
+    },
+    {
+        pictureLink: happybirthday5,
+        id: 9,
+        category: "normal",
+        Reso: {
+            width: 736,
+            height: 1308
+        },
+        PictureLocation: [
+            {
+                id: 1,
+                location: {
+                    left: 402,
+                    top: 455, rotate: -6
+                },
+                reso: {
+                    width: 240,
+                    height: 225
+                }
+            }, {
+                id: 2,
+                location: {
+                    left: 101,
+                    top: 628, rotate: -22
+                },
+                reso: {
+                    width: 280,
+                    height: 260
+                }
+            }
+
+        ],
+    },
+    {
+        pictureLink: happybirthday6,
+        id: 10,
+        category: "normal",
+        Reso: {
+            width: 736,
+            height: 1308
+        },
+        PictureLocation: [
+            {
+                id: 1,
+                location: {
+                    left: 358,
+                    top: 265, rotate: 9
+                },
+                reso: {
+                    width: 190,
+                    height: 190
+                }
+            },
+             {
+                id: 2,
+                location: {
+                    left: 93,
+                    top: 435, rotate: -15
+                },
+                reso: {
+                    width: 246,
+                    height: 246
+                }
+            },     {
+                id: 3,
+              location: {
+                    left: 432,
+                    top: 598, rotate: 9
+                },
+                reso: {
+                    width: 203,
+                    height: 203
+                }
+            }
+
+        ],
+    },
+
 ];
 export default birthdayimg;
