473ce6265caa178c9f9024012d4534a63d129ab3
kundal kumar <kundal5335@gmail.com>
2025-10-01 11:28:24 +0530

    upload to github

--- src/components/frame-layout.js
+++ src/components/frame-layout.js
@@ -9,6 +9,7 @@ const Framelaout = () => {
     const params = useParams();
     const { id } = params;
     const canvasRef = useRef(null);
+    const fileInputRef = useRef(null);
     const [Canvase, setCanvase] = useState(null);
     const [selected, setSelected] = useState(null);
 
@@ -33,7 +34,12 @@ const Framelaout = () => {
                     console.log(Maincanvas);
                     Maincanvas.renderAll();
                     setCanvase(Maincanvas);
-                    Maincanvas.toDataURL('image/png');
+                    Maincanvas.toDataURL('image/png');  
+                    const activeObj = Maincanvas.getActiveObject();
+                    if (activeObj) {
+                        console.log(activeObj);
+                        console.log("isoajdooajj  eie qieqw");
+                    }
                 }
                 return () => Maincanvas.dispose();
             }
@@ -48,7 +54,6 @@ const Framelaout = () => {
                 frame.PictureLocation.find((loca) => {
                     if (loca.id === pictureId) {
                         FabricImage.fromURL(URL.createObjectURL(file)).then((img) => {
-
                             img.set({
                                 top: loca.location.top,
                                 left: loca.location.left,
@@ -87,10 +92,43 @@ const Framelaout = () => {
         link.download = "edited-canvas.png";
         link.click();
     };
+    function deleteActiveObject() {
+        const activeObj = Canvase.getActiveObject();
+        console.log(activeObj);
+        if (activeObj) {
+            Canvase.remove(activeObj);
+        }
 
+    }
+    function changePicture() {
+        fileInputRef.current.click();
+    }
+    function handlenewfile(e, { selected }, { picture }) {
+        deleteActiveObject();
+        const file = e.target.files[0];
+        FabricImage.fromURL(URL.createObjectURL(file)).then((img) => {
+            img.set({
+                top: selected.top,
+                left: selected.left,
+                scaleX: picture.reso.width / img.width,
+                scaleY: picture.reso.height / img.height,
+                lockScalingX: true,
+                lockScalingY: true,
+                lockSkewingX: true,
+                lockSkewingY: true,
+                lockRotation: true,
+                lockMovementX: true,
+                lockMovementY: true,
+            })
+            if (picture.location.rotate) {
+                img.rotate(picture.location.rotate);
+            }
+
+            Canvase.add(img); Canvase.renderAll();
+        })
+    }
     return (
         <div style={{ marginLeft: "100px", marginRight: "100px" }}>
-
             <div>
                 <canvas ref={canvasRef} style={{ border: "1px solid blue" }} />
                 {
@@ -115,26 +153,34 @@ const Framelaout = () => {
             </button>
             {selected && (
                 birthdayimg.find((frame) => frame.id === parseInt(id))?.PictureLocation.map((picture) => (
-                    < div className="main" key={picture.id} style={{ position: "absolute", top: `${picture.location.top}px`, left: `${picture.location.left}px` }}>
-                        <div className="up">
-                            <button className="card1 btn btn-success">
-                                <i className="bi bi-crop"></i> Crop
-                            </button>
-                            <button className="card2 btn btn-success">
-                                <i className="bi bi-cloud-upload"></i> Change
-                            </button>
+                    (selected.top === picture.location.top && selected.left === picture.location.left) ? (
+                        < div className="main" key={picture.id} style={{ position: "absolute", top: `${picture.location.top}px`, left: `${picture.location.left}px` }}>
+                            <div className="up">
+                                <button className="card1 btn btn-success" >
+                                    <i className="bi bi-crop"></i> Crop
+                                </button>
+                                <button className="card2 btn btn-success" onClick={changePicture}>
+                                    <i className="bi bi-cloud-upload"></i> Change
+                                </button>
+                                <input type="file"
+                                    ref={fileInputRef}
+                                    style={{ display: "none" }}
+                                    onChange={(e) => handlenewfile(e, { selected }, { picture })} />
+                            </div>
+                            <div className="down">
+                                <button className="card3 btn btn-success" onClick={deleteActiveObject}>
+                                    <i className="bi bi-trash3"></i>Delete{selected.top}
+                                </button>
+                                <button className="card4 btn btn-success">
+                                    <i className="bi bi-check-circle"></i>
+                                </button>
+                            </div>
                         </div>
-                        <div className="down">
-                            <button className="card3 btn btn-success">
-                                <i className="bi bi-check-circle"></i>
-                            </button>
-                            <button className="card4 btn btn-success">
-                                <i className="bi bi-check-circle"></i>
-                            </button>
-                        </div>
-                    </div>
+                    ) : (0)
+
                 )
                 ))}
+            {console.log(selected)}
         </div >
 
     );
