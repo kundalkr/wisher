90264fc7b47e58148bdf907767aa1c7013ac1f08
kundal kumar <kundal5335@gmail.com>
2025-10-04 12:16:03 +0530

    working on somethings

--- src/components/frame-layout.js
+++ src/components/frame-layout.js
@@ -186,7 +186,7 @@ const Framelaout = () => {
                     if (frame.id === parseInt(id)) {
                         frame.PictureLocation.find((picture) =>
                         (selected.top === picture.location.top || (Math.trunc(selected.top) === (picture.location.rotate ? picture.location.input.top : picture.location.top)) ?
-                            (<div className="main" key={picture.id} style={{ position: "absolute", top: `${picture.location.top}px`, left: `${picture.location.left}px`,zIndex:1111111 }}>
+                            (<div className="main" key={picture.id} style={{ position: "absolute", top: `${picture.location.top}px`, left: `${picture.location.left}px`, zIndex: 1111111 }}>
                                 <div className="up">
                                     <button className="card1" id="myBtn" onClick={modalOn}>
                                         <i className="bi bi-crop"></i> Crop
@@ -210,7 +210,6 @@ const Framelaout = () => {
                                 <div className="down">
                                     <button className="card3 btn btn-success" onClick={deleteActiveObject}>
                                         <i className="bi bi-trash3"></i>Delete
-                                        {/* {console.log(selected)} */}
                                     </button>
                                     <button className="card4 btn btn-success">
                                         <i className="bi bi-check-circle"></i>
