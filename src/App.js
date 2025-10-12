f42266dedb4ffab1d4b003c52305e8a639f2ae3d
kundal kumar <kundal5335@gmail.com>
2025-10-12 10:29:05 +0530

    mgcmprsr added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import react from "react"
 import ReactDOM from "react-dom/client"
 import Birthdayframe from "./components/birthday/birthdayframe";
 import Framelaout from "./components/frame-layout"
@@ -6,13 +5,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "../node_modules/react-cropper/node_modules/cropperjs/dist/cropper.css"
 import Error from "./components/Error";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
-import ResponsiveCanvas from "../src/components/test"
+// import ResponsiveCanvas from "../src/components/test"
 
 const Applayout = () => {
   return (<>
       <Birthdayframe />
-  {/* <ResponsiveCanvas/> */}
-
   </>)
 };
 
